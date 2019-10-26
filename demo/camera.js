c23192d675cbbe14de1756bba96d4dd9648a41f7
quangthuyhoang <qtnhoang@gmail.com>
2019-10-26 18:04:29 -0500

    add camera file

---
+++ demo/camera.js
@@ -0,0 +1,46 @@
+var video = document.querySelector("#videoElement");
+
+var front = false;
+
+const constraints = {
+  video: true
+}
+
+const uppdatedConstrants = front ? constraints : 
+  {
+  ...constraints,
+  video : {
+    facingMode: { exact: "environment" },
+    width: { 
+      min: 1280,
+      ideal: 1920,
+      max: 2560,
+    },
+    height: {
+      min: 720,
+      ideal: 1080,
+      max: 1440
+    },
+  }
+}
+
+if (navigator.mediaDevices.getUserMedia) {
+    console.log(navigator.mediaDevices)
+    navigator.mediaDevices.enumerateDevices()
+    .then(e => {
+      e.forEach(device => {
+        if(device.kind = "videoinput") {
+          console.log(device)
+        }
+      }) 
+    })
+    // console.log()
+  navigator.mediaDevices.getUserMedia(uppdatedConstrants)
+    .then(function (stream) {
+        console.log("stream", stream)
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}
